f210f9746cdc1e7fcced86c75b246373541067cd
chanduch1999 <chanduch1999@gmail.com>
2019-10-01 21:29:56 +0530

    check

---
+++ majority.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var flag,ind,t,a,i,n int
+	fmt.Scan(&t)
+	for t>0 {
+		fmt.Scan(&n)
+		 flag =0 
+		 ind=-1
+		s :=make([]int ,100)
+		for i=0;i<n;i++ {
+			fmt.Scan(&a)
+			s[a]++
+		}
+		for i=0;i<n;i++ {
+			if s[i]>=n{
+				flag = 1
+				ind =i
+			}
+		}
+		if(flag==1){
+			fmt.Println(s[ind])
+		}else {
+			fmt.Println(-1)
+		}
+		t=t-1
+	}
+}
