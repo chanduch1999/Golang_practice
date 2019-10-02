f78bf7b57c0edcdfb17eadfdd308fbea8f451928
chanduch1999 <chanduch1999@gmail.com>
2019-10-02 14:34:44 +0530

    second

---
+++ 397_a.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+
+
+var a,b,d,rem,sum int
+    fmt.Scan( &a,&b)
+     sum = a;
+    for a >= b{
+        d = a / b
+        sum += d
+        rem=a % b
+		a = d+ rem  
+	  }
+	fmt.Println(sum)
+}
